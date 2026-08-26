bae10d93043bf0ccaa0e78c712cae32a29340702
Allison Chanin <allison.chanin@gmail.com>
2026-08-26 15:49:32 -0700

    Iteration F2: Creating a Hotwired Cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,11 +28,12 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
+        format.turbo_stream
         format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
       else
-        format.html { render :new, status: :unprocessable_content }
-        format.json { render json: @line_item.errors, status: :unprocessable_content }
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
   end
